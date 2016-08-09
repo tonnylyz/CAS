365afaf4bddf0ddc87a1baef67ab11a1d8a9fe75
tonnylyz <me@lyzde.com>
2016-08-09 22:09:31 +0800

    Add missing files

---
+++ App_Code/User.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data;
+using System.Web;
+namespace CAS
+{
+    /// <summary>
+    /// CAS User
+    /// </summary>
+    public class User
+    {
+        private int ID;
+        public string UUID;
+        public string username;
+        private string password;
+        private string SN;
+        public string realname;
+        public string mail;
+        public string phone;
+        public string QQ;
+        public DateTime birthday;
+
+        private string permissionString;
+        public User(string username)
+        {
+            this.username = username;
+            SqlIntegrate si = new SqlIntegrate(Utility.connStr);
+            si.InitParameter(1);
+            si.AddParameter("@username", SqlIntegrate.DataType.VarChar, username, 50);
+            DataRow dr = si.Reader("SELECT * FROM CAS_User WHERE username = @username");
+            ID = Convert.ToInt32(dr["ID"]);
+            UUID = dr["UUID"].ToString();
+            password = dr["pwd"].ToString();
+            realname = dr["name"].ToString();
+            SN = dr["SN"].ToString();
+            mail = dr["mail"].ToString();
+            phone = dr["phone"].ToString();
+            QQ = dr["QQ"].ToString();
+            birthday = Convert.ToDateTime(dr["birthday"].ToString());
+            permissionString = dr["permission"].ToString();
+        }
+        public static User Current
+        {
+            get
+            {
+                if (HttpContext.Current.Session["User"] != null)
+                    return (User)HttpContext.Current.Session["User"];
+                else
+                    return null;
+            }
+            set
+            {
+                HttpContext.Current.Session["User"] = value;
+            }
+        }
+
+        public enum PermissionType
+        {
+            BULLETIN = 0,
+            CALENDAR = 1,
+            FILEUPLD = 2,
+            PHOTOSUB = 3,
+            HOMEWORK = 4,
+            TALKSUBT = 5
+        }
+        public int Permission(PermissionType type)
+        {
+            string[] permission = permissionString.Split(',');
+            return int.Parse(permission[(int)type]);
+        }
+
+        public static bool IsLogin
+        {
+            get
+            {
+                return !(Current == null);
+            }
+        }
+        private bool Verify(string password)
+        {
+            return (this.password == Utility.Encrypt(password));
+        }
+        public bool Login(string password)
+        {
+            if (Verify(password))
+            {
+                Current = this;
+                return true;
+            }
+            else
+                return false;
+        }
+        public void Logout()
+        {
+            Current = null;
+        }
+        public bool SetPassword(string password, string passwordNew)
+        {
+            if (Verify(password))
+            {
+                SqlIntegrate si = new SqlIntegrate(Utility.connStr);
+                si.Execute("UPDATE CAS_User SET pwd = '" + Utility.Encrypt(passwordNew) + "' WHERE UUID = '" + UUID + "'");
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ App_Code/Utility.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace CAS
+{
+    /// <summary>
+    /// CAS 网站配置以及公用函数
+    /// </summary>
+    public class Utility
+    {
+        public static string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+        public static void Log(string message)
+        {
+            SqlIntegrate si = new SqlIntegrate(connStr);
+            si.InitParameter(5);
+            si.AddParameter("@context", SqlIntegrate.DataType.Text, message);
+            si.AddParameter("@IP", SqlIntegrate.DataType.VarChar, HttpContext.Current.Request.UserHostAddress, 50);
+            si.AddParameter("@browser", SqlIntegrate.DataType.Text, HttpContext.Current.Request.UserAgent);
+            si.AddParameter("@OS", SqlIntegrate.DataType.VarChar, HttpContext.Current.Request.Browser.Platform, 50);
+            si.AddParameter("@session", SqlIntegrate.DataType.VarChar, User.IsLogin ? User.Current.username : "未登录用户", 50);
+            si.Execute("INSERT INTO Log ([context], [IP], [browser], [OS], [session]) VALUES (@context, @IP, @browser, @OS, @session)");
+        }
+        public static void Log(Exception message)
+        {
+            Log(message.Message + message.StackTrace);
+        }
+        public static string Encrypt(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] data = Encoding.Default.GetBytes(password);
+            byte[] md5data = md5.ComputeHash(data);
+            md5.Clear();
+            string str = "";
+            for (int i = 0; i < md5data.Length - 1; i++)
+                str += md5data[i].ToString("X").PadLeft(2, '0');
+            return str;
+        }
+        public static string String2JSON(string str)
+        {
+            str = str.Replace(">", "&gt;");
+            str = str.Replace("<", "&lt;");
+            str = str.Replace(" ", "&nbsp;");
+            str = str.Replace("\"", "&quot;");
+            str = str.Replace("\'", "&#39;");
+            str = str.Replace("\\", "\\\\");
+            str = str.Replace("\n", "\\n");
+            str = str.Replace("\r", "\\r");
+            return str;
+        }
+        public static void MakeSmallImg(System.IO.Stream fromFileStream, string fileSaveUrl, double templateWidth, double templateHeight)
+        {
+            try
+            {
+                System.Drawing.Image myImage = System.Drawing.Image.FromStream(fromFileStream, true);
+                double newWidth = myImage.Width, newHeight = myImage.Height;
+                if (myImage.Width > myImage.Height || myImage.Width == myImage.Height)
+                {
+                    if (myImage.Width > templateWidth)
+                    {
+                        newWidth = templateWidth;
+                        newHeight = myImage.Height * (newWidth / myImage.Width);
+                    }
+                }
+                else
+                {
+                    if (myImage.Height > templateHeight)
+                    {
+                        newHeight = templateHeight;
+                        newWidth = myImage.Width * (newHeight / myImage.Height);
+                    }
+                }
+                System.Drawing.Size mySize = new System.Drawing.Size((int)newWidth, (int)newHeight);
+                System.Drawing.Image bitmap = new System.Drawing.Bitmap(mySize.Width, mySize.Height);
+                System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bitmap);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                g.Clear(System.Drawing.Color.White);
+                g.DrawImage(myImage, new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                new System.Drawing.Rectangle(0, 0, myImage.Width, myImage.Height),
+                System.Drawing.GraphicsUnit.Pixel);
+
+                bitmap.Save(fileSaveUrl, System.Drawing.Imaging.ImageFormat.Jpeg);
+                g.Dispose();
+                myImage.Dispose();
+                bitmap.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+            }
+        }
+    }
+}
